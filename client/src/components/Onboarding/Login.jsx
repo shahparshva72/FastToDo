e69250c6a02ff825195d2535a24ca8ddca8848e6
Parshva <36695274+shahparshva72@users.noreply.github.com>
2023-10-14 20:15:30 -0400

    Modified Onboarding UI

--- client/src/components/Onboarding/Login.jsx
+++ client/src/components/Onboarding/Login.jsx
@@ -52,13 +52,27 @@ const Login = () => {
 
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-800 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8 bg-gray-700 p-6 rounded-lg shadow-lg">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
-            Sign in to your account
+    <div className="min-h-screen flex items-center justify-between bg-gradient-to-r from-gray-800 to-gray-900 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="text-center">
+        <h1 className="text-white text-6xl font-bold tracking-widest mb-4">
+          Task Manager
+        </h1>
+        <p className="text-white text-2xl font-semibold italic">
+          Organize Tomorrow, Today.
+        </p>
+      </div>
+
+
+      <div className="max-w-md w-full space-y-8 bg-white p-4 rounded-lg shadow-lg">
+        <div className="text-center p-6">
+          <h2 className="mt-4 text-4xl font-semibold text-gray-800">
+            Sign In to your account.
           </h2>
+          <h3 className="mt-2 text-xl text-gray-600">
+            Access and manage your tasks seamlessly.
+          </h3>
         </div>
+
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
@@ -70,8 +84,8 @@ const Login = () => {
                 name="username"
                 type="text"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Username"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:z-10 sm:text-sm"
+                placeholder="Enter your username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
@@ -85,33 +99,36 @@ const Login = () => {
                 name="password"
                 type="password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Password"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-gray-500 focus:border-gray-500 focus:z-10 sm:text-sm"
+                placeholder="Enter your password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </div>
 
-          <div className="flex justify-between items-center">
+          <div className="flex flex-col items-center">
             <button
               type="submit"
-              className="group relative w-1/2 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-2"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600 mb-2"
             >
               Sign In
             </button>
             <button
               type="button"
               onClick={goToRegister}
-              className="group relative w-1/2 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+              className="group relative w-full flex justify-center py-2 px-4 border border-white text-sm font-medium rounded-md text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
             >
-              Register
+              Don't have an account? Register here
             </button>
           </div>
+
+
         </form>
       </div>
     </div>
   );
+
 };
 
 export default Login;
--- client/src/components/Onboarding/Register.jsx
+++ client/src/components/Onboarding/Register.jsx
@@ -53,40 +53,57 @@ const Register = () => {
 
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-800 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8 text-white">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
-            Create an account to start managing your tasks and stay organized.
+    <div className="min-h-screen flex items-center justify-between bg-gradient-to-r from-gray-800 to-gray-900 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="text-center">
+        <h1 className="text-white text-6xl font-bold tracking-widest mb-4">
+          Task Manager
+        </h1>
+        <p className="text-white text-2xl font-semibold italic">
+          Organize Tomorrow, Today.
+        </p>
+      </div>
+      <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-lg shadow-lg">
+        <div className="text-center p-6">
+          <h2 className="mt-4 text-4xl font-semibold text-gray-800">
+            Join Task Manager
           </h2>
+          <h3 className="mt-2 text-xl text-gray-600">
+            Start managing your tasks and stay organized.
+          </h3>
         </div>
         <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-          <div className="rounded-md shadow-sm -space-y-px">
-            <div>
+          <div className="rounded-md shadow-sm space-y-4">
+            <div className="relative">
               <label htmlFor="username" className="sr-only">
                 Username
               </label>
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <i className="fas fa-user text-gray-400"></i>
+              </div>
               <input
                 id="username"
                 name="username"
                 type="text"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
             </div>
-            <div>
+            <div className="relative">
               <label htmlFor="password" className="sr-only">
                 Password
               </label>
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <i className="fas fa-lock text-gray-400"></i>
+              </div>
               <input
                 id="password"
                 name="password"
                 type="password"
                 required
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
@@ -94,23 +111,26 @@ const Register = () => {
             </div>
           </div>
 
-          <div>
+          <div className="flex flex-col items-center space-y-4">
             <button
               type="submit"
-              className="group relative w-full flex justify-center p-4 border border-transparent text-md font-semibold rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600 transition duration-200"
             >
               Register
             </button>
+            <button
+              onClick={goToLogin}
+              className="group relative w-full flex justify-center py-2 px-4 border border-white text-sm font-medium rounded-md text-white bg-gray-700 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 transition duration-200"
+            >
+              Sign In Instead
+            </button>
           </div>
         </form>
-        <div className="mt-4">
-          <button className="text-white bg-purple-500 p-4 rounded-md hover:bg-purple-700 text-md" onClick={goToLogin}>
-            Sign In Instead
-          </button>
-        </div>
       </div>
+
     </div>
   );
+
 };
 
 export default Register;
